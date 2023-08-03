03afb8358a4bd9844605a4ef3115e8d4c9c38b65
Henry Núñez Suarez <47455232+Arkarok@users.noreply.github.com>
2023-08-03 11:26:49 -0500

    Seccion 3: Componentes

---
+++ BlazorPeliculas/BlazorPeliculas/Client/Repositorios/Repositorio.cs
@@ -0,0 +1,29 @@
+﻿using BlazorPeliculas.Shared.Entidades;
+
+namespace BlazorPeliculas.Client.Repositorios
+{
+    public class Repositorio : Irepositorio
+    {
+        public List<Pelicula> ObtenerPeliculas()
+        {
+                return new List<Pelicula>()
+                {
+                new Pelicula
+                {
+                    Titulo = "Wakanda Forever",
+                    FechaLanzamiento = new DateTime(2022, 11, 11)
+                },
+                new Pelicula
+                {
+                    Titulo = "Mohana",
+                    FechaLanzamiento = new DateTime(2016, 11, 23)
+                },
+                new Pelicula
+                {
+                    Titulo = "Inception",
+                    FechaLanzamiento = new DateTime(2023, 07, 31)
+                }
+            };
+        }
+    }
+}
