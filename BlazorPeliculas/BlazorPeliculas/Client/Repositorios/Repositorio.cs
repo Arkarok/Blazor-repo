07c8a6557518f175e21f00b5a7e00c2764d537c0
Henry Núñez Suarez <47455232+Arkarok@users.noreply.github.com>
2023-08-04 16:42:04 -0500

    Seccion 5: Formularios

--- BlazorPeliculas/BlazorPeliculas/Client/Repositorios/Repositorio.cs
+++ BlazorPeliculas/BlazorPeliculas/Client/Repositorios/Repositorio.cs
@@ -11,19 +11,19 @@ namespace BlazorPeliculas.Client.Repositorios
                 new Pelicula
                 {
                     Titulo = "Wakanda Forever",
-                    FechaLanzamiento = new DateTime(2022, 11, 11),
+                    Lanzamiento = new DateTime(2022, 11, 11),
                     Poster = "https://th.bing.com/th/id/OIP.d-e92xz1a36-EFTBszzzKQAAAA?pid=ImgDet&rs=1"
                 },
                 new Pelicula
                 {
                     Titulo = "Moana",
-                    FechaLanzamiento = new DateTime(2016, 11, 23),
+                    Lanzamiento = new DateTime(2016, 11, 23),
                     Poster = "https://th.bing.com/th/id/OIP.X1emm8zndOf3_N7deHf7OwHaLH?pid=ImgDet&rs=1"
                 },
                 new Pelicula
                 {
                     Titulo = "Inception",
-                    FechaLanzamiento = new DateTime(2023, 07, 31),
+                    Lanzamiento = new DateTime(2023, 07, 31),
                     Poster = "https://th.bing.com/th/id/R.dd9827fbb0199cd20694739d54c346d8?rik=5JsQjhudqNqO9w&pid=ImgRaw&r=0"
                 }
             };
