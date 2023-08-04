5539126dad0be0b84c356b2f07afc75d70d4d0ad
Henry Núñez Suarez <47455232+Arkarok@users.noreply.github.com>
2023-08-04 12:04:40 -0500

    Seccion 5: Formularios (Formulario de Peliculas)

--- BlazorPeliculas/BlazorPeliculas/Client/Program.cs
+++ BlazorPeliculas/BlazorPeliculas/Client/Program.cs
@@ -1,5 +1,7 @@
 using BlazorPeliculas.Client;
 using BlazorPeliculas.Client.Repositorios;
+using CurrieTechnologies.Razor.SweetAlert2;
+using MathNet.Numerics;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +10,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<Irepositorio, Repositorio>();
+ConfigureServices(builder.Services);
 
 await builder.Build().RunAsync();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<Irepositorio, Repositorio>();
+    services.AddSweetAlert2();
+}
