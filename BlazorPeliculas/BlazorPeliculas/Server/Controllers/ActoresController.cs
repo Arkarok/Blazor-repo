e9862a1fdfb800163a64045407be5d8220586b8e
Arkarok <47455232+Arkarok@users.noreply.github.com>
2023-08-06 13:33:15 -0500

    Seccion 6: Preparando el proyecto para que guarde imagenes II

--- BlazorPeliculas/BlazorPeliculas/Server/Controllers/ActoresController.cs
+++ BlazorPeliculas/BlazorPeliculas/Server/Controllers/ActoresController.cs
@@ -1,4 +1,5 @@
-﻿using BlazorPeliculas.Shared.Entidades;
+﻿using BlazorPeliculas.Server.Helpers;
+using BlazorPeliculas.Shared.Entidades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorPeliculas.Server.Controllers
@@ -8,15 +9,25 @@ namespace BlazorPeliculas.Server.Controllers
     public class ActoresController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IAlmacenadorArchivos _almacenadorArchivos;
+        private readonly string _contenedor = "personas";
 
-        public ActoresController(ApplicationDbContext context)
+        public ActoresController(ApplicationDbContext context, IAlmacenadorArchivos almacenadorArchivos)
         {
             _context = context;
+            _almacenadorArchivos = almacenadorArchivos;
         }
 
         [HttpPost]
         public async Task<ActionResult<int>> Post(Actor actor)
         {
+
+            if (!string.IsNullOrWhiteSpace(actor.Foto))
+            {
+                var fotoActor = Convert.FromBase64String(actor.Foto);
+                actor.Foto = await _almacenadorArchivos.GuardarArchivo(fotoActor, "jpg", _contenedor);
+            }
+
             _context.Add(actor);
             await _context.SaveChangesAsync();
             return actor.Id;
